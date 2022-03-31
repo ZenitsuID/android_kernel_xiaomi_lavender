c97f2dc5b46fea37bccac4e8ca7d99615100a9a1
Zenitsu Prjkt™ <101568999+ZenitsuID@users.noreply.github.com>
2022-03-31 21:50:21 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,288 +1,160 @@
-#!/usr/bin/env bash
+#! /bin/bash
 
 #
-# Script For Building Android Kernel
+# Script for building Android arm64 Kernel
+#
+# Copyright (c) 2021 Fiqri Ardyansyah <fiqri15072019@gmail.com>
+# Based on Panchajanya1999 script.
 #
 
-MODEL="Redmi Note 7"
+# Set environment for directory
+KERNEL_DIR=$PWD
+IMG_DIR="$KERNEL_DIR"/out/arch/arm64/boot
 
-DEFCONFIG=lavender-perf_defconfig
+# Get defconfig file
+DEFCONFIG=vendor/lavender-perf_defconfig
 
-KERNEL_DIR=${PWD}
-
-IMAGE=${PWD}/out/arch/arm64/boot/Image.gz-dtb
-
-# create config file for build variant info
-echo $@ > my.conf
-conf=${KERNEL_DIR}/my.conf
-
-# Specify compiler.
-# 'aosp', 'azure' or 'gcc'
-if grep "aosp" $conf; then
-COMPILER=aosp
-  if grep "full-lto" $conf; then
-		if grep "oldcam" $conf; then
-			CCACHE_BRANCH=aosp-13-oldcam
-		elif grep "newcam" $conf; then
-			CCACHE_BRANCH=aosp-13-newcam
-		elif grep "qti" $conf; then
-			CCACHE_BRANCH=aosp-13-qti
-		fi
-  else
-	if grep "oldcam" $conf; then
-		CCACHE_BRANCH=aosp-13-thin-oldcam
-	elif grep "newcam" $conf; then
-		CCACHE_BRANCH=aosp-13-thin-newcam
-	elif grep "qti" $conf; then
-		CCACHE_BRANCH=aosp-13-thin-qti
-	fi
-  fi
-elif grep "azure" $conf; then
-COMPILER=azure
-	if grep "oldcam" $conf; then
-		CCACHE_BRANCH=azure-oldcam
-	elif grep "newcam" $conf; then
-		CCACHE_BRANCH=aazure-newcam
-	elif grep "qti" $conf; then
-		CCACHE_BRANCH=azure-qti
-	fi
-  else
-	if grep "oldcam" $conf; then
-		CCACHE_BRANCH=azure-thin-oldcam
-	elif grep "newcam" $conf; then
-		CCACHE_BRANCH=azure-thin-newcam
-	elif grep "qti" $conf; then
-		CCACHE_BRANCH=azure-thin-qti
-	fi
-  fi
-elif grep "gcc" $conf; then
-COMPILER=gcc
-CCACHE_BRANCH=gcc-
-if grep "oldcam" $conf; then
-	CCACHE_BRANCH=gcc-oldcam
-elif grep "newcam" $conf; then
-	CCACHE_BRANCH=gcc-newcam
-elif grep "qti" $conf; then
-	CCACHE_BRANCH=gcc-qti
-fi
-fi
-
-# enable FULL_LTO (default THIN_LTO)
-if grep "full-lto" $conf; then
-	sed -i 's/CONFIG_THINLTO=y/# CONFIG_THINLTO is not set/' arch/arm64/configs/lavender-perf_defconfig
-fi
-
-# disable LTO
-if grep "no-lto" $conf; then
-	sed -i 's/CONFIG_LTO=y/# CONFIG_LTO is not set/' arch/arm64/configs/lavender-perf_defconfig
-	sed -i 's/CONFIG_LTO_CLANG=y/# CONFIG_LTO_CLANG is not set/' arch/arm64/configs/lavender-perf_defconfig
-	sed -i 's/# CONFIG_LTO_NONE is not set=y/CONFIG_LTO_NONE=y/' arch/arm64/configs/lavender-perf_defconfig
-fi
-
-# Specify built variant
-# 'oldcam', 'newcam' or 'qti'
-if grep "qti" $conf; then
-TYPE=QTI
-elif grep "oldcam" $conf; then
-TYPE=Oldcam
-elif grep "newcam" $conf; then
-TYPE=Newcam
-fi
-
-# Verbose build
-VERBOSE=0
-
-export PROCS=$(nproc --all)
-
-# Set Date
-DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
-START=$(date +"%s")
-TANGGAL=$(date +"%F-%S")
-
-# Commit Head
-COMMIT_HEAD=$(git log --oneline -1)
-
-# Kernel Version
-KERVER=$(make kernelversion)
-
-function download_ccache() {
-  if grep "ccache" $conf; then
-  echo "|| Downloading CCACHE ||"
-	git config --global user.name ZenitsuID
-	git config --global user.email zenitsuxd5@gmail.com
-  xd_info "Downloading CCACHE..."
-	git clone --depth=1 https://ZenitsuID:$gh_token@github.com/ZenitsuID/drone_ccache_backups.git -b ${CCACHE_BRANCH} ${CCACHE_BRANCH}
-	xd_info "Exported CCACHE $(du ${KERNEL_DIR}/${CCACHE_BRANCH} -sh)"
-  export CCACHE_DIR=${KERNEL_DIR}/${CCACHE_BRANCH}
-  export CCACHE_EXEC=$(which ccache)
-  export USE_CCACHE=1
-  ccache -M 8G
-  ccache -z
-  fi
-}
-
-function upload_ccache() {
-  if grep "ccache" $conf; then
-  echo "|| Uploading CCACHE ||"
-	cd ${KERNEL_DIR}/${CCACHE_BRANCH}
-	# 7za a -tzip -v45m ${TYPE}.zip ccache_repo/* -sdel
-	xd_info "Collecting CCACHE Backup..."
-	git add -f -A
-	git commit -sm "CCACHE: IMPORT CCACHE FROM NEW BUILD"
-  xd_info "Uploading CCACHE Backup..."
-	git push -f
-	xd_info "Uploaded!"
-	fi
-}
-
-
-# Send info plox channel
-function xd_info() {
-	# dont send thi msg for all 3 build variant, (oldcam/newcam/qti)
-	if grep "oldcam" $conf; then
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="$1"
-	fi
-}
-
-clone() {
-	echo " Cloning Dependencies "
-	if [[ $COMPILER = "gcc" ]]
-	then
-		echo "|| Cloning GCC ||"
-		git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git/ -b gcc-new gcc64
-		git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git/ -b gcc-new gcc32
-    export PATH=${KERNEL_DIR}/gcc64/bin/:${KERNEL_DIR}/gcc32/bin/:/usr/bin:$PATH
-    export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/gcc64/bin/aarch64-elf-gcc --version | head -n 1)
-	elif [[ $COMPILER = "azure" ]]
-	then
-		echo  "|| Cloning Azure Clang-14 ||"
-		git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git/ clang
-    export PATH=${KERNEL_DIR}/clang/bin:$PATH
-    export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
-  elif [[ $COMPILER = "aosp" ]]
-	then
-    echo "|| Cloning AOSP-13 ||"
-    git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r433403 -b 11.0 --depth=1 clang
-    git clone https://github.com/sohamxda7/llvm-stable.git/ -b gcc64 --depth=1 gcc
-    git clone https://github.com/sohamxda7/llvm-stable.git/ -b gcc32 --depth=1 gcc32
-    export PATH=${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}
-    export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
-  fi
-	echo "|| Cloning Anykernel ||"
-	git clone --depth=1 https://github.com/who-em-i/AnyKernel3.git AnyKernel3
-}
-
-# Export
-function exports() {
+# Set environment for etc.
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_BUILD_HOST=DroneCI
+export KBUILD_BUILD_VERSION="1"
 export KBUILD_BUILD_USER="ZenitsuID"
+export KBUILD_BUILD_HOST="DroneCI"
+
+# Set environment for telegram
+export CHATID="-1001726055194"
+export token=$TELEGRAM_TOKEN
+export BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
+export BOT_BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
+
+#
+# Set if do you use GCC or clang compiler
+# Default is clang compiler
+#
+COMPILER=gcc
+
+# Get distro name
+DISTRO=$(source /etc/os-release && echo ${NAME})
+
+# Get all cores of CPU
+PROCS=$(nproc --all)
+export PROCS
+
+# Set Date and time
+DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
+
+# Get branch name
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+export BRANCH
+
+# Check kernel version
+KERVER=$(make kernelversion)
+
+# Get last commit
+COMMIT_HEAD=$(git log --oneline -1)
+
+# Set function for telegram
+tg_post_msg() {
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$CHATID" \
+	-d "disable_web_page_preview=true" \
+	-d "parse_mode=html" \
+	-d text="$1"
 }
 
-# Send info plox channel
-function sendinfo() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="<b>•  bEast Kernel  •</b>%0ABuild started on <code>Drone CI</code>%0AFor device <b>Xiaomi Redmi Note7/7S</b> (lavender)%0ABranch: <code>$(git rev-parse --abbrev-ref HEAD)</code>%0A<b>Kernel Version : </b><code>$KERVER</code>%0ACompiler Used: <code>${KBUILD_COMPILER_STRING}</code>%0AType: <code>#${TYPE}</code>%0A<b>COMMIT_HEAD : </b><a href='$DRONE_COMMIT_LINK'>$COMMIT_HEAD</a>%0A<b>Build Status:</b>#HMP-Beta"
+tg_post_build() {
+	# Post MD5 Checksum alongwith for easeness
+	MD5CHECK=$(md5sum "$1" | cut -d' ' -f1)
+
+	# Show the Checksum alongwith caption
+	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
+	-F chat_id="$CHATID"  \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=html" \
+	-F caption="$2 | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"
 }
-# Push kernel to channel
-function push() {
-    cd AnyKernel3
-    ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
-        -F chat_id="$chat_id" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Redmi Note 7/7s (lavender)</b> | <b>${KBUILD_COMPILER_STRING}</b>"
+
+# Set function for cloning repository
+clone() {
+	if [[ $COMPILER == "clang" ]]; then
+		# Clone Proton clang
+		git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
+		# Set environment for clang
+		TC_DIR=$KERNEL_DIR/clang
+		# Get path and compiler string
+		KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+		PATH=$TC_DIR/bin/:$PATH
+	elif [[ $COMPILER == "gcc" ]]; then
+		# Clone GCC ARM64 and ARM32
+		git clone https://github.com/fiqri19102002/aarch64-gcc.git -b elf-gcc-10-tarballs --depth=1 gcc64
+		git clone https://github.com/fiqri19102002/arm-gcc.git -b elf-gcc-10-tarballs --depth=1 gcc32
+		# Set environment for GCC ARM64 and ARM32
+		GCC64_DIR=$KERNEL_DIR/gcc64
+		GCC32_DIR=$KERNEL_DIR/gcc32
+		# Get path and compiler string
+		KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1)
+		PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
+	fi
+	
+	export PATH KBUILD_COMPILER_STRING
 }
-# Fin Error
-function finerr() {
-    LOG=error.log
-   curl -F document=@$LOG "https://api.telegram.org/bot$token/sendDocument" \
-        -F chat_id="$chat_id" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="Build throw an error(s)"
-    exit 1
+
+# Set function for naming zip file
+set_naming() {
+	KERNEL_NAME="STRIX-ginklow-unified-personal-$DATE"
+	export ZIP_NAME="$KERNEL_NAME.zip"
 }
-# Compile plox
-function compile() {
-	if [[ $COMPILER = "azure" ]]
-	then
-		make O=out ARCH=arm64 ${DEFCONFIG}
-		make -j$(nproc --all) O=out \
-				ARCH=arm64 \
-				CC="ccache clang" \
-				LD=ld.lld \
+
+# Set function for starting compile
+compile() {
+	echo -e "Kernel compilation starting"
+	tg_post_msg "<b>Docker OS: </b><code>$DISTRO</code>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device : </b><code>Redmi Note 8/8T (ginkgo/willow)</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$BRANCH</code>%0A<b>Last Commit : </b><code>$COMMIT_HEAD</code>%0A<b>Status : </b>#Personal"
+	make O=out "$DEFCONFIG"
+	BUILD_START=$(date +"%s")
+	if [[ $COMPILER == "clang" ]]; then
+		make -j"$PROCS" O=out \
+				CROSS_COMPILE=aarch64-linux-gnu- \
+				CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+				CC=clang \
 				AR=llvm-ar \
 				NM=llvm-nm \
-				OBJCOPY=llvm-objcopy \
+				LD=ld.lld \
 				OBJDUMP=llvm-objdump \
-				STRIP=llvm-strip \
-				V=$VERBOSE \
-				CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-				CROSS_COMPILE=aarch64-linux-gnu- 2>&1 | tee error.log
-	elif [[ $COMPILER = "gcc" ]]
-	then
-	        make O=out ARCH=arm64 ${DEFCONFIG}
-	        make -j$(nproc --all) O=out \
-			        ARCH=arm64 \
-			        CROSS_COMPILE=aarch64-elf- \
-      				CC="ccache gcc" \
-			        LD=aarch64-elf-ld.lld \
-			        AR=aarch64-elf-ar \
-			        OBJDUMP=aarch64-elf-objdump \
-			        STRIP=aarch64-elf-strip \
-			        V=$VERBOSE \
-			        CROSS_COMPILE_ARM32=arm-eabi- | tee error.log
-	elif [[ $COMPILER = "aosp" ]]
-	then
-	        make O=out ARCH=arm64 ${DEFCONFIG}
-	        make -j$(nproc --all) O=out \
-      				ARCH=arm64 \
-      				CLANG_TRIPLE=aarch64-linux-gnu- \
-      				CROSS_COMPILE=aarch64-linux-android- \
-      				CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-      				CC="ccache clang" \
-      				LD=ld.lld \
-      				AR=llvm-ar \
-      				NM=llvm-nm \
-      				OBJCOPY=llvm-objcopy \
-      				OBJDUMP=llvm-objdump \
-      				READELF=llvm-readelf \
-      				OBJSIZE=llvm-size \
-      				STRIP=llvm-strip \
-      				HOSTCC=clang \
-              V=$VERBOSE \
-      				HOSTCXX=clang++ | tee error.log
+				STRIP=llvm-strip
+	elif [[ $COMPILER == "gcc" ]]; then
+		export CROSS_COMPILE_ARM32=$GCC32_DIR/bin/arm-eabi-
+		make -j"$PROCS" O=out CROSS_COMPILE=aarch64-elf-
 	fi
+	BUILD_END=$(date +"%s")
+	DIFF=$((BUILD_END - BUILD_START))
+	if [ -f "$IMG_DIR"/Image.gz-dtb ] 
+	then
+		echo -e "Kernel successfully compiled"
+	elif ! [ -f "$IMG_DIR"/Image.gz-dtb ]
+	then
+		echo -e "Kernel compilation failed"
+		tg_post_msg "<b>Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>"
+		exit 1
+	fi
+}
 
-    if ! [ -a "$IMAGE" ]; then
-        finerr
-        exit 1
-    fi
-    cp $IMAGE AnyKernel3
+# Set function for zipping into a flashable zip
+gen_zip() {
+	# Move kernel and DTBO image to flasher aka AnyKernel3
+	mv "$IMG_DIR"/Image.gz-dtb flasher/Image.gz-dtb
+	mv "$IMG_DIR"/dtbo.img flasher/dtbo.img
+	cd flasher || exit
+
+	# Archive to flashable zip
+	zip -r9 "$ZIP_NAME" * -x .git README.md *.zip
+
+	# Prepare a final zip variable
+	ZIP_FINAL="$ZIP_NAME"
+
+	tg_post_build "$ZIP_FINAL" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
+	cd ..
 }
-# Zipping
-function zipping() {
-    cd AnyKernel3 || exit 1
-    zip -r9 bEast-HMP-${TYPE}-Kernel-${TANGGAL}.zip * -x README.md .git
-    cd ..
-}
+
 clone
-sendinfo
-download_ccache
-exports
 compile
-zipping
-END=$(date +"%s")
-DIFF=$(($END - $START))
-push
-upload_ccache
+set_naming
+gen_zip
